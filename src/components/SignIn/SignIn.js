dd103f8ee2c870db156e21cc53226944f5fdf06f
Daniel Kennedy <fade.rocks@gmail.com>
2021-04-17 18:02:59 +0100

    serve on keroku2

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -17,7 +17,7 @@ class SignIn extends React.Component {
   }
 
 onSubmitSignIn = () => {
-  fetch('http://localhost:3000/signin', {
+  fetch('https://intense-fjord-57856.herokuapp.com/signin', {
     method: 'post',
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({
